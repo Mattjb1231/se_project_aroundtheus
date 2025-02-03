1da7743bdf925c43062d1ee678dc9aeb5b21f3fb
Matthew Barriera <mattjb1231@gmail.com>
2025-02-02 22:30:43 -0500

    new

--- scripts/index.js
+++ scripts/index.js
@@ -64,6 +64,7 @@ function handleProfileEditSubmit(e) {
   e.preventDefault();
   profileTitle.textContent = profileTitleInput.value;
   profileDescription.textContent = profileDescriptionInput.value;
+  closeModal();
 }
 
 profileEditBtn.addEventListener("click", () => {
